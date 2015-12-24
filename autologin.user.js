fb5e69515cd1280a78e3de9266a3a4910acb235f
dsin <phong.ek@gmail.com>
2015-12-24 23:16:50 +0700

    add function

--- autologin.user.js
+++ autologin.user.js
@@ -38,12 +38,44 @@ var current_url = url_o.hostname+url_o.pathname;
 if(current_url in pages){
     var page = pages[current_url];
 
-    document.getElementById(page['username_node_id']).value = page['username'];
-    document.getElementById(page['password_node_id']).value = page['password'];
+    if('username_node_id' in page){
+		document.getElementById(page['username_node_id']).value = page['username'];
+	} else if('username_node_name' in page){
+		if('form_node_name' in page){
+			document.forms[page['form_node_name']].elements[page['username_node_name']].value = page['username'];
+		} else {
+			document.forms[0].elements[page['username_node_name']].value = page['username'];
+		}
+	}
+	
+	if('password_node_id' in page){
+		document.getElementById(page['password_node_id']).value = page['password'];
+	} else if('password_node_name' in page){
+		if('form_node_name' in page){
+			document.forms[page['form_node_name']].elements[page['password_node_name']].value = page['password'];
+		} else {
+			document.forms[0].elements[page['password_node_name']].value = page['password'];
+		}
+	}
+	
+	if('other_inputs' in page){
+		var other_inputs = page['other_inputs'];
+		for(i in other_inputs){
+			document.forms[0].elements[other_inputs[i]['input_name']].value = other_inputs[i]['input_value'];
+		}
+	}
+	
     if('submit_button_node_id' in page){
         document.getElementById(page['submit_button_node_id']).click();    
     } else if('submit_form_node_id' in page){
         document.getElementById(page['submit_form_node_id']).submit();    
-    }
+    } else {
+		if('form_node_name' in page){
+			document.forms[page['form_node_name']].submit();
+		} else {
+			// submit whatever form
+			document.forms[0].submit();
+		}
+	}
 }
 
